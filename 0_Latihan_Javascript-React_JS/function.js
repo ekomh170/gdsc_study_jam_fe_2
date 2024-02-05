ab9e014720938773ccc9b5657aa7cbf038fff273
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-02-05 20:51:27 +0700

    merubah nama folder

---
+++ 0_Latihan_Javascript-React_JS/function.js
@@ -0,0 +1,16 @@
+function hitungTambah(){
+    console.log(1 + 3);
+}
+
+function hitungKurang(){
+    console.log(2 - 1);
+}
+
+
+const hitungBagi = () =>{
+    console.log(4 / 2);
+}
+
+hitungTambah();
+hitungKurang(); 
+hitungBagi();
